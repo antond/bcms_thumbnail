816ede031420ef2644c50470a4e34032bc3801d5
Anton Djurasaj <anton.djurasaj@gmail.com>
2012-03-09 11:29:58 -0500

    Forgot to add files to previous commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BcmsThumbnail::Application.config.secret_token = '5f989675418f67b9e04d1650ef20b90cf97e40c36a39fe7c370682c6de3afb84d95d33fe3d1ad64ed3795e1b6d12ab937060057271474fc972e55a091a956408'
---
+++ lib/bcms_thumbnail/version.rb
@@ -0,0 +1,3 @@
+module BcmsContentRotator
+  VERSION = "1.0.5"
+end
